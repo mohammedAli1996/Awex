77fe8b122247063d9992777f5dbeee0a2cc627da
Mohammed Altujjar <mohammed_.1996@live.com>
2022-05-02 20:46:47 +0400

    Hire Generation Link changed to domain name

--- src/main/resources/static/js/interview/generateHire.js
+++ src/main/resources/static/js/interview/generateHire.js
@@ -2,6 +2,6 @@
 
 function generateHireForm() {
     var position = document.getElementById("Position").value ; 
-    document.getElementById("generatedLink").href = "http://159.223.235.65:8080/addHire/"+position ; 
+    document.getElementById("generatedLink").href = "http://crm.awex.ae/addHire/"+position ; 
     document.getElementById("generatedLink").innerHTML = "Link Generated"; 
 }
