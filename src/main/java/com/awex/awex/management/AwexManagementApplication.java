f1afbb3e858fd9911b2bef4b4bbedb19230e8de0
Mohammed Altujjar <mohammed_.1996@live.com>
2022-05-09 11:38:29 +0400

    HR Manager Now can control Users

--- src/main/java/com/awex/awex/management/AwexManagementApplication.java
+++ src/main/java/com/awex/awex/management/AwexManagementApplication.java
@@ -9,6 +9,6 @@ public class AwexManagementApplication {
 	public static void main(String[] args) {  
 		SpringApplication.run(AwexManagementApplication.class, args);    
 	}
-  
+    
 }       
-                         
+                              
